8a4a50788be55f0203c88f64219592b2c2407806
hakaton2019 <47497992+hakaton2019@users.noreply.github.com>
2019-02-14 11:47:18 +0300

    delete paginator

--- pets2.php
+++ pets2.php
@@ -223,26 +223,6 @@
         }
     ?>
     </div>
-
-    <!-- Pagination -->
-    <ul class="pagination justify-content-center">
-        <!--li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="sr-only">Previous</span>
-            </a>
-        </li-->
-        <li class="page-item">
-            <a class="page-link" href="pets2.php">1</a>
-        </li>
-        <!--li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-                <span class="sr-only">Next</span>
-            </a>
-        </li-->
-    </ul>
-
 </div>
 <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
     <div class="container text-center">
